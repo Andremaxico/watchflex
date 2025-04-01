ba08935f6a2f95f7fa65f4f0e1d249455fc6239a
Andriy <andremaxico321@gmail.com>
2025-04-01 22:47:39 +0300

    added parse from stream to object

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,4 +1,5 @@
 import { RequestTokenResponseDataModel, SessionDataViewModel, SessionResponseDataModel } from "@/models";
+import { readableStreamToObject } from "@/utils/server/readableStreamToObject";
 import { NextRequest, NextResponse } from "next/server"
 
 
@@ -39,35 +40,37 @@ export const POST = async (req: NextRequest) => {
 	// const searchParams = new URLSearchParams(reqUrl.searchParams)
 	// const requestToken = searchParams.get('request_token');
 
-	const reader = req.body?.getReader();
-	const result = await reader?.read();
+	if(req.body) {
+		const body = await readableStreamToObject(req.body);
+	
+		const url = `https://api.themoviedb.org/3/authentication/session/new`;
+		const options = {
+			method: 'POST',
+			headers: {
+				accept: 'application/json',
+				'content-type': 'application/json',
+				Authorization: `Bearer ${process.env.API_READ_ACCESS_TOKEN}`,
+			},
+			body: JSON.stringify(body)
+		};
+	
+		const response = await fetch(url, options);
+		const data: SessionResponseDataModel = await response.json();
 
-	console.log('reqyest body', result);
-
-	const url = `https://api.themoviedb.org/3/authentication/session/new`;
-	const options = {
-		method: 'POST',
-		headers: {
-			accept: 'application/json',
-			'content-type': 'application/json',
-			Authorization: `Bearer ${process.env.API_READ_ACCESS_TOKEN}`,
-		},
-		// body: JSON.stringify(requestBody)
-	};
-
-	const response = await fetch(url, options);
-	const data: SessionResponseDataModel = await response.json();
-
-	console.log('auth data', data);
-
-	if(data.success) {
-		return NextResponse.json({
-			success: data.success,
-			id: data.session_id,
-		})
+		
+		if(data.success) {
+			return NextResponse.json({
+				success: data.success,
+				id: data.session_id,
+			})
+		} else {
+			return NextResponse.json({
+				success: false,
+			}, {status: response.status})
+		}
 	} else {
 		return NextResponse.json({
 			success: false,
-		}, {status: response.status})
+		}, {status: 502})
 	}
 }
